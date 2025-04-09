96b58ce74ecf874b9b925ecf9231870aa9f05ae7
OWO Devs <developers.owo@gmail.com>
2025-04-09 09:24:47 +0200

    feat: added bepinplugin

--- OWO_H3VR.cs
+++ OWO_H3VR.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace OWO_H3VR
 {
-
+    [BepInPlugin("org.bepinex.plugins.OWO_H3VR", "OWO_H3VR", "1.0.0")]
     public class Plugin : BaseUnityPlugin //CAMBIAR LAS REFERENCIAS DE UNITY POR LAS DEL JUEGO
     {
 
