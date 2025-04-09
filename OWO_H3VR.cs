f56d35b61020a2f22bf5930aee7517362b265599
OWO Devs <developers.owo@gmail.com>
2025-04-09 09:20:50 +0200

    feat: added bepinex owoskin and harmony without errors

--- OWO_H3VR.cs
+++ OWO_H3VR.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace OWO_H3VR
 {
-    public class OWO_H3VR
+
+    public class Plugin : BaseUnityPlugin //CAMBIAR LAS REFERENCIAS DE UNITY POR LAS DEL JUEGO
     {
+
+        #pragma warning disable CS0109
+        internal static new ManualLogSource Log;
+        #pragma warning restore CS0109
+
+        public static OWOSkin owoSkin;
+
+        private void Awake()
+        {
+            Log = Logger;
+            Logger.LogMessage("OWO_REPO plugin is loaded!");
+            owoSkin = new OWOSkin();
+
+            var harmony = new Harmony("owo.patch.repo");
+            harmony.PatchAll();
+        }
+
     }
 }
