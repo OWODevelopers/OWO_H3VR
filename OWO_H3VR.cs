1eb40f459ec84a8487dfdb77e2582ae66dd79e08
OWO Devs <developers.owo@gmail.com>
2025-04-22 14:28:13 +0200

    chore: clean some code

--- OWO_H3VR.cs
+++ OWO_H3VR.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 namespace OWO_H3VR
 {
     [BepInPlugin("org.bepinex.plugins.OWO_H3VR", "OWO_H3VR", "1.0.0")]
-    public class Plugin : BaseUnityPlugin //CAMBIAR LAS REFERENCIAS DE UNITY POR LAS DEL JUEGO
+    public class Plugin : BaseUnityPlugin
     {
 
 #pragma warning disable CS0109
@@ -44,15 +44,12 @@ namespace OWO_H3VR
             {
                 if (!owoSkin.CanFeel()) return;
 
-                //bool activeForegrip = false;
-                //try { activeForegrip = __instance.Foregrip.activeSelf; }
-                //catch (Exception e) { }
+
 
                 string sensation = SensationsDictionary.SensationByWeaponType(__instance.RoundType);
 
-                //if (sensation == "Pistol") owoSkin.LOG($"### -> ARMA NO REGISTRADA?? - {__instance.RoundType}");
 
-                if (twoHandStabilized || foregripStabilized) //dejar como ultimo paso a comprobar
+                if (twoHandStabilized || foregripStabilized) 
                 {
                     owoSkin.Feel($"{sensation} LR");
                 }
