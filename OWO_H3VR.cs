585386ee662aafd6befbd103bd8773ab998312d1
OWO Devs <developers.owo@gmail.com>
2025-04-22 14:35:09 +0200

    feat: grapple pull player method

--- OWO_H3VR.cs
+++ OWO_H3VR.cs
@@ -262,5 +262,20 @@ namespace OWO_H3VR
         }
 
         #endregion
+
+
+        [HarmonyPatch(typeof(GrappleGun), "AttemptRetract")]
+        public class OnAttemptRetract
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance, bool Pull)
+            {
+                if (!owoSkin.CanFeel()) return;
+                if (Pull && __instance.IsHeld)
+                {
+                    owoSkin.LOG($"Grapple me lleva");
+                }
+            }
+        }
     }
 }
